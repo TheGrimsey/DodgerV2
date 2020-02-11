86a98e3708d5efe11b0dea3dbdfd8334647267e3
TheGrimsey <2411768-TheGrimsey@users.noreply.gitlab.com>
2020-02-11 20:34:34 +0100

    Added AsteroidSpawner and made movement system actually work.
    To do that we changed .NET 4.0 to .NET Standard 2.0 and added CopyTransformToGameObject component to Asteroid prefab

---
+++ DodgerV2/Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    public GameObject AsteroidPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 100; i++)
+        {
+            Vector3 position = new Vector3((i - 50) * 1.1f, 0);
+
+            Instantiate(AsteroidPrefab, position, Quaternion.identity);
+        }
+    }
+}
