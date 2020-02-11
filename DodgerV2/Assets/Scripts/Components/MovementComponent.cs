0e4150be22aa6ef3bc0c06bbbcf1948c7fea07f0
TheGrimsey <2411768-TheGrimsey@users.noreply.gitlab.com>
2020-02-11 19:22:00 +0100

    Begin work on movementsystems.
    Also added basic sprites

---
+++ DodgerV2/Assets/Scripts/Components/MovementComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Unity.Entities;
+using UnityEngine;
+
+[Serializable]
+[GenerateAuthoringComponent]
+public struct MovementComponent : IComponentData
+{
+    public float MovementSpeed;
+    public Vector2 RelativeMovementDirection;
+}
---
+++ DodgerV2/Assets/Scripts/Systems/MovementSystem.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class MovementSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        //Cache this.
+        var deltaTime = Time.DeltaTime;
+
+        //For every entity with a MovementComponent.
+        Entities.ForEach((ref Translation translation, ref MovementComponent movementComponent) =>
+        {
+            //Calculate movement delta
+            Vector3 deltaPositionVec3 = movementComponent.MovementSpeed * movementComponent.RelativeMovementDirection * deltaTime;
+
+            float3 deltaPositionf3 = new float3(deltaPositionVec3.x, deltaPositionVec3.y, deltaPositionVec3.z);
+
+            translation.Value += deltaPositionf3;
+
+        });
+
+    }
+}
