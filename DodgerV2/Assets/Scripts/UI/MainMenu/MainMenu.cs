9388906ac11c9a03d90f5913ed3b2944460f6306
Grim <TheGrimsey@users.noreply.github.com>
2020-02-19 09:35:02 +0100

    Implement simple mainmenu.

---
+++ DodgerV2/Assets/Scripts/UI/MainMenu/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public ScoreKeeper scoreKeeper;
+
+    public Text highScoreText;
+    public Text lastScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        highScoreText.text = scoreKeeper.HighScore.ToString();
+
+        lastScoreText.text = scoreKeeper.LastScore.ToString();
+    }
+}
